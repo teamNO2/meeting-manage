0b8ae63ab3c4a1d6b0552814c589f8e840fe3df1
sunkeqiang <1830716357@qq.com>
2019-12-21 18:40:08 +0800

    修改Sign实体类---孙克强

--- meeting/src/main/java/com/suixingpay/entity/Sign.java
+++ meeting/src/main/java/com/suixingpay/entity/Sign.java
@@ -8,6 +8,7 @@
  */
 package com.suixingpay.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -39,6 +40,7 @@ public class Sign {
     /** 是否报名 **/
     private Integer signIsapply;
     /** 签到时间 **/
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date signTime;
     /** 用户集合 **/
